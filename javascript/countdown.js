3a64bbf67da424da75bf474abe0a52457b3bf4d4
cisco7755 <100170316+cisco7755@users.noreply.github.com>
2022-04-26 19:16:20 +0100

    csmcchoirconvention.com

---
+++ javascript/countdown.js
@@ -0,0 +1,8 @@
+const futuredate = new Date(2022, 6, 10, 11, 30, 0 );
+const nowdate = new Date();
+const gap = futuredate - nowdate;
+console.log(gap);
+
+
+
+
---
+++ javascript/scrip.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+
+const ulsection = document.getElementById
+
+
+hamburger.addEventListener('click', ()=>{
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+    
+    
+});
+
